c315f2d4862554ec9c85fd686c9af9299eb2d5ca
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2024-04-11 03:46:54 +0200

    Working memory tools return result

--- src/memory/working_memory.py
+++ src/memory/working_memory.py
@@ -5,14 +5,23 @@ class WorkingMemory:
     def __init__(self) -> None:
         self.store = {}
 
-    def add(self, key: str, value: str):
-        self.store[key] = value
+    def add(self, key: str, value: str) -> str:
+        if key not in self.store:
+            self.store[key] = value
+            return f"{key}:{value} added succesfully"
+        return f"Error: key {key} already exists"
 
-    def update(self, key: str, value: str):
-        self.store[key] = value
+    def update(self, key: str, value: str) -> str:
+        if key in self.store:
+            self.store[key] = value
+            return f"{key}:{value} updated succesfully"
+        return f"Error: key {key} does not exist"
 
-    def delete(self, key: str):
-        del self.store[key]
+    def delete(self, key: str) -> str:
+        if key in self.store:
+            del self.store[key]
+            return f"{key} deleted succesfully"
+        return f"Error: key {key} does not exist"
 
     def __str__(self) -> str:
         return json.dumps(self.store)
