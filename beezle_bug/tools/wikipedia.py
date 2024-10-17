fe28a1327bece67252548ec6be30db7ea4dbb44b
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2024-10-17 09:08:45 +0200

    Add wikipedia tools
    * Add search tool
    * Add summary tool

---
+++ beezle_bug/tools/wikipedia.py
@@ -0,0 +1,27 @@
+from pydantic import Field
+from beezle_bug.tools import Tool
+
+import wikipedia
+
+
+class SearchWikipedia(Tool):
+    """
+    Do a Wikipedia search for query
+    """
+
+    query: str = Field(..., description="the search query")
+    results: int = Field(..., description="the maxmimum number of results returned")
+
+    def run(self, agent):
+        return wikipedia.search(self.query, results=self.results)
+
+
+class GetWikipediaPageSummary(Tool):
+    """
+    Get a plain text summary of a Wikipedia page.
+    """
+
+    query: str = Field(..., description="the search query")
+
+    def run(self, agent):
+        return wikipedia.summary(self.query, auto_suggest=False)
